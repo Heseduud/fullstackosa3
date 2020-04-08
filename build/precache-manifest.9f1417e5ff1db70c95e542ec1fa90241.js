ec5d1e71c636b3cc037aaca5b111e7776014a6fc
Santeri Nurminen <nusanteri@gmail.com>
2020-04-08 14:03:44 +0300

    uibuild

---
+++ build/precache-manifest.9f1417e5ff1db70c95e542ec1fa90241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f574298cc44f23b01b2fca430f18e44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13b756c470887a8bbe85",
+    "url": "/static/css/main.2f84b0f1.chunk.css"
+  },
+  {
+    "revision": "f6915c6258628ffbe4b1",
+    "url": "/static/js/2.9a17e31d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a17e31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b756c470887a8bbe85",
+    "url": "/static/js/main.c6b94621.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
